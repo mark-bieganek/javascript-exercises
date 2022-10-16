ce6b99bd26e1b4f8988181054a76fea6fc1e53ae
Mark Bieganek <markbieganek@gmail.com>
2022-10-15 22:53:30 -0600

    Add solutions for exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let output = '';
+    // If count is not a negative number.
+    if (count >= 0) {
+        // Repeat the input string by (count) times.
+        for (i = 1; i <= count; i++) {
+            output += string;
+        }
+    } else {
+        // Return 'ERROR' because count is a negative number.
+        output = 'ERROR';
+    };
+    return output;
 };
 
 // Do not edit below this line
