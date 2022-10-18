1559024c3b13fa42d5df1f515d63e088dbd6d079
Mark Bieganek <markbieganek@gmail.com>
2022-10-18 09:27:43 -0600

    Plan constraints and initial algorithm

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,5 +2,30 @@ const sumAll = function() {
 
 };
 
+/*
+Implement a function that takes 2 integers and returns the sum of every
+number between(and including) them:
+
+```javascript
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+```
+
+CONSTRAINTS:
+1. Get the sum of two integers.
+2. As integers, the numbers should not have a fractional component,
+    and don't support decimal points.
+
+ALGORITHM:
+# Create a way to remember the result of our operations.
+# Check the first two inputs.
+    # If input is not a number, assign it a default value of 0.
+    # Otherwise, convert input to integer.
+    # Add these inputs. Store the result.
+# Output the result.
+
+PROGRAM:
+
+*/
+
 // Do not edit below this line
 module.exports = sumAll;
