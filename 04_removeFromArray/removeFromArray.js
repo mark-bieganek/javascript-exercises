cf7afd1e54840f934046b3e22df7d98e4a21034a
Mark Bieganek <markbieganek@gmail.com>
2022-10-17 22:38:54 -0600

    Add fix to pass test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray = [''], value1) {
+    // Set sourceArray from the provided argument.
+    let sourceArray = Array.isArray(inputArray) ? inputArray: Array[''];
+    
+    // Remove the requested element from the array.
+    //const output = sourceArray.filter(value => value !== arguments[1]);
 
+    // Create a list of values we will 
+    
+    for (argument = 0; argument <= arguments.length; argument++) {
+        let argument = arguments[1];
+        let output = sourceArray.filter(value => value !== arguments[1]);
+        sourceArray = output;
+    }
+    // Output the result.
+    return sourceArray;
 };
 
 // Do not edit below this line
