0e190b71ee211074873de977dc4f9a86d24ef365
Mark Bieganek <markbieganek@gmail.com>
2022-10-17 20:37:52 -0600

    Fix for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+// This function takes an input string and returns a reversed string.
+const reverseString = function(string) {
+    let output = '';
+    // Iterate in reverse through the string.
+    for (i = (string.length -1); i >= 0; i--) {
+        // Add this letter to the output string.
+        output += string.at(i);
+    };
+    return output;
 };
 
 // Do not edit below this line
