077837b343a5bfc13ab6922ddc17fbc851e75ac1
Mark Bieganek <markbieganek@gmail.com>
2022-10-20 08:58:44 -0600

    Add comments w/contraints & operations

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -6,6 +6,32 @@ const ctof = function() {
 
 };
 
+
+/*
+Write two functions that convert temperatures from Fahrenheit to 
+Celsius, and vice versa:
+```
+ftoc(32) // fahrenheit to celsius, should return 0
+
+ctof(0) // celsius to fahrenheit, should return 32
+```
+
+Because we are human, we want the result temperature to be rounded to 
+one decimal place: i.e., `ftoc(100)` should return `37.8` and not 
+`37.77777777777778`.
+
+
+CONSTRAINTS:
+1. The inputs can only be numerical.
+2. The outputs will only be numerical, accurate to 1 decimal place.
+
+OPERATIONS:
+1. Convert a given number representing a temperature in °F to °C.
+2. Convert a given number representing a temperature in °C to °F.
+3. Ensure the given value is a number.
+
+*/
+
 // Do not edit below this line
 module.exports = {
   ftoc,
