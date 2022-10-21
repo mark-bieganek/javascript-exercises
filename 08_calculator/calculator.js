e1f737903051e168a79b76df49dae1f7ef83454b
Mark Bieganek <markbieganek@gmail.com>
2022-10-20 21:39:23 -0600

    Add algorithm for addition calculation.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -22,6 +22,35 @@ const factorial = function() {
 	
 };
 
+/* 
+ * Create a calculator that does the following:
+ * - add
+ * - subtract
+ * - get the sum
+ * - multiply
+ * - get the power
+ * - find the factorial
+ * 
+ * CONSTRAINTS:
+ * (add)
+ * 1. A minimum of two numbers must be provided.
+ * 2. Non-numerical inputs are not accepted.
+ * 
+ * OPERATIONS:
+ * (add)
+ * - Validate the given values as numbers.
+ * - Add the values together.
+ * - Output the result.
+ * 
+ * ALGORITHM:
+ * (add)
+ * - Create a variable named sum with a default value of 0.
+ * - Iterate through the given parameter values.
+ *    - Validate given value as a number. Return an error if not a number.
+ *    - Add this value to the current sum.
+ * - Return the result.
+ */
+
 // Do not edit below this line
 module.exports = {
   add,
